32b8a86f3bbe86561f62052db27148bfff7cc3ba
nhardiansa <nhardiansa@gmail.com>
2021-08-05 22:42:45 +0800

    membuat web components dengan vanilla js

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,53 +1,9 @@
-// ========== Function ==========
-
-// function DataSource(onSuccess, onFailed) {
-//     this.onSuccess = onSuccess;
-//     this.onFailed = onFailed;
-// }
-
-// DataSource.prototype.searchClub = function (keyword) {
-//     const filteredClubs = clubs.filter(club => {
-//         return club.name.toUpperCase().includes(keyword.toUpperCase());
-//     });
-
-//     if (filteredClubs.length) {
-//         this.onSuccess(filteredClubs);
-//     } else {
-//         this.onFailed(`${keyword} is not found`);
-//     }
-// };
-
-
-// ========== Class Function ==========
-
-// class DataSource {
-//     constructor(onSuccess, onFailed) {
-//         this.onSuccess = onSuccess;
-//         this.onFailed = onFailed;
-//     }
-
-//     searchClub(keyword) {
-//         const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
-
-//         if (filteredClubs.length) {
-//             this.onSuccess(filteredClubs);
-//         } else {
-//             this.onFailed(`${keyword} is not found`);
-//         }
-//     }
-// }
+import clubs from "./clubs.js";
 
 //========== Class Function with Promise ==========
 
 class DataSource {
     static searchClub(keyword) {
-        // const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
-
-        // if (filteredClubs.length) {
-        //     this.onSuccess(filteredClubs);
-        // } else {
-        //     this.onFailed(`${keyword} is not found`);
-        // }
         return new Promise((resolve, reject) => {
             const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
 
@@ -59,3 +15,5 @@ class DataSource {
         });
     }
 }
+
+export default DataSource;
